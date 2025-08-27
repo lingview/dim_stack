b0278e9db15d69f96b3081b1b2618314f25268a4
lingview <official@lingview.xyz>
2025-08-27 20:50:32 +0800

    修改vite开发服务器配置，改为绑定所有地址

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-      react(),
+    react(),
     tailwindcss(),
   ],
   server: {
+    host: '0.0.0.0',
     port: 5173,
     proxy: {
       '^/api/.*': {
