96ca86a42bb94cbcbaaa4e16e8c4b1be7ca7c8b2
lingview <official@lingview.xyz>
2025-09-01 14:27:44 +0800

    修改测试文件名称

--- backend/src/test/java/xyz/lingview/dimstack/test/ArticleTagAndCategoryTest.java
+++ backend/src/test/java/xyz/lingview/dimstack/test/ArticleTagAndCategoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class ArticleTagCategoryTest {
+public class ArticleTagAndCategoryTest {
 
     @Autowired
     private ArticleTagMapper articleTagMapper;
