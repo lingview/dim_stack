7584dcc5c00e7cd6d127df5b47fa74598168416f
lingview <official@lingview.xyz>
2025-09-01 14:20:31 +0800

    修改站点设置模块数据库字段映射

--- backend/src/main/java/xyz/lingview/dimstack/domain/SiteConfig.java
+++ backend/src/main/java/xyz/lingview/dimstack/domain/SiteConfig.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class SiteConfig {
     private Integer id;
-    private String siteName;
-    private String registerUserPermission;
+    private String site_name;
+    private String register_user_permission;
     private String copyright;
 }
