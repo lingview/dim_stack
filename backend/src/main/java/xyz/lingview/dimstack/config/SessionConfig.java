bf1214fb82aecbcce684863107f65c25da646c28
lingview <official@lingview.xyz>
2025-09-04 21:25:38 +0800

    增加cookie持久化配置

---
+++ backend/src/main/java/xyz/lingview/dimstack/config/SessionConfig.java
@@ -0,0 +1,22 @@
+package xyz.lingview.dimstack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setSameSite("Lax");
+        serializer.setCookiePath("/");
+        serializer.setCookieMaxAge(7 * 24 * 60 * 60);
+
+        return serializer;
+    }
+}
