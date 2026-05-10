c8112bcd149528905a1a0c498ffc3590d0715a56
lingview <official@lingview.xyz>
2026-05-10 12:52:29 +0800

    fix(scripts): 修复git历史记录导出脚本直接运行不生效问题
    修复git历史记录导出脚本直接运行不生效问题

--- scripts/git_history.bat
+++ scripts/git_history.bat
@@ -1,3 +1,4 @@
+@echo off
 git config --global i18n.logoutputencoding gbk
-git log --pretty=format:"%h %an %ad %s" > commits.txt
+git log --pretty=format:"%%h %%an %%ad %%s" > commits.txt
 git config --global --unset i18n.logoutputencoding
